be0fea50d48c3f854744aa2806635cc6610ccc73
Luis Fernando Benatto <luisbenatto5@gmail.com>
2022-06-21 00:14:32 -0300

    Ignite Lab | Aula 01 - Inicio da especializacao em ReactJS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-5xl font-bold text-violet-500">Hello, Ignite Lab!</h1>
+      {/* <h1 className="title">Hello, Ignite Lab!</h1> */}
+    </div>
+  )
+}
+
+export default App
+
+// CMS = Content Management System
